5b51f1106c969babda1030e73f5b919465ff057c
RobertFrundin <robert.frundin@gmail.com>
2022-11-13 19:53:07 +0300

    finished game page & player component

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,9 +1,18 @@
 import styles from './game.module.scss'
+import copyImage from '../../assets/copy.svg'
+import { Player } from '../../components/Player/Player'
 export const Game = () => {
   return (
     <div className={styles.wrap}>
       <main className={styles.content}>
-        <div className={styles.players}></div>
+        <div className={styles.players}>
+          <ul>
+            <Player></Player>
+            <Player></Player>
+            <Player></Player>
+            <Player></Player>
+          </ul>
+        </div>
         <div className={styles.board}>
           <h2>Текущее слово:</h2>
           <h1>
@@ -11,7 +20,12 @@ export const Game = () => {
           </h1>
           <input defaultValue="A" type="text" />
           <button>Отправить</button>
+          <span className={styles.copy}>
+            <img src={copyImage} alt="" />
+            <span>#8fdad7</span>
+          </span>
         </div>
+        <button className={styles.close}></button>
       </main>
     </div>
   )
