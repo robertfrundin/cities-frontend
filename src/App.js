eda55d900eaa5487e79c21a8dcbce3bc6dad1db2
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-17 17:30:18 +0300

    added loader screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-import { Rooms } from './pages/Rooms/Rooms'
+// import { Game } from './pages/Game/Game'
+import { Search } from './pages/Search/Search'
+// import { Rooms } from './pages/Rooms/Rooms'
 // import { Auth } from './pages/Auth/Auth'
 // import { Profile } from './pages/Profile/Profile'
 function App() {
   return (
     <div className="App">
-      <Rooms/>
+    <Search/>
+
     </div>
 
   )
--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -10,23 +10,22 @@ export const Rooms = ({
   roomsCount
 }) => {
   return (
-    
-      <div className={styles.roomsList}>
-        <div className={roomstyles.room}>
-          <img alt="Avatar" className = {roomstyles.avatar} src={roomLogo}/>
-          <div className={roomstyles.roomId}>Cоздать игру</div>
-          <button className={roomstyles.joinButton}></button>
-        </div>
-        <Room playerCount={12} roomSize= {13} roomId = {`#2ad932`}/>
-        <Room playerCount={2} roomSize= {3} roomId = {`#2ad932`}/>
-        <Room playerCount={8} roomSize= {9} roomId = {`#2ad932`}/>
-
-        <Room playerCount={5} roomSize= {5} roomId = {`#2ad932`}/>
-        <Room playerCount={6} roomSize= {6} roomId = {`#2ad932`}/>
-        <Room playerCount={8} roomSize= {9} roomId = {`#2ad932`}/>
-        <Room playerCount={12} roomSize= {12} roomId = {`#2ad932`}/>
-      
+    <div className={styles.roomsList}>
+      <div className={roomstyles.room}>
+        <img alt="Avatar" className = {roomstyles.avatar} src={roomLogo}/>
+        <div className={roomstyles.roomId}>Cоздать игру</div>
+        <button className={roomstyles.joinButton}></button>
       </div>
-  
+      <Room playerCount={12} roomSize= {13} roomId = {`#2ad932`}/>
+      <Room playerCount={2} roomSize= {3} roomId = {`#2ad932`}/>
+      <Room playerCount={8} roomSize= {9} roomId = {`#2ad932`}/>
+
+      <Room playerCount={5} roomSize= {5} roomId = {`#2ad932`}/>
+      <Room playerCount={6} roomSize= {6} roomId = {`#2ad932`}/>
+      <Room playerCount={8} roomSize= {9} roomId = {`#2ad932`}/>
+      <Room playerCount={12} roomSize= {12} roomId = {`#2ad932`}/>
+    
+    </div>
+
   )
 }
