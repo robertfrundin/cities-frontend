3ed8cd533e8409a9591a9a21648af5bf843bc1a9
robert <robert.frundin@gmail.com>
2022-11-20 17:52:50 +0300

    finished user grpc service

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,17 +2,19 @@ import { Button } from "../../components/Button/Button"
 import { ChangeLang } from "../../components/ChangeLang/ChangeLang"
 import AuthPhoto from '../../assets/hippopotamus-svgrepo-com.svg'
 import styles from './auth.module.scss'
+import getUserData from "../../grpc-services/user-service/service";
 export const Auth = () => {
+  const user={name:'oleg'}
   return (
     <>
     <div className={styles.wrap}>
       <main className={styles.content}>
         <div className ={styles.selectLogin} >
-          <button className = {styles.guest}> ГОСТЬ </button>
-          <button className = {styles.login}> ВОЙТИ </button>
+          <button  className = {styles.guest}> ГОСТЬ </button>
+          <button onClick={getUserData.bind(user)} className = {styles.login}> ВОЙТИ </button>
         </div>
         <img alt="Avatar"className = {styles.avatar} src={AuthPhoto}/>
-        <input className = {styles.input}></input>
+        {/*<input className = {styles.input}></input>*/}
         <div className={styles.authBlock}>
           <Button text = {'ИГРАТЬ'} type = 'play' size = 'medium'> </Button> 
           <ChangeLang/>
