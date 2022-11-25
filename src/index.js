eb9e9dacdb4df14ac27ec34c0d7020f9b753e313
Ruslan Kaziev <rrkaziev@gmail.com>
2022-11-25 11:57:49 +0300

    added 404 page and some button styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth } from "./pages/Auth/Auth";
 import { Rooms } from "./pages/Rooms/Rooms";
 import { Game } from "./pages/Game/Game";
+import { Page404 } from "./pages/Page404/Page404";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +16,7 @@ root.render(
       <Route path="auth" element={<Auth />}></Route>
       <Route path="rooms" element={<Rooms />}></Route>
       <Route path="game/:gameid" element={<Game />}></Route>
+      <Route path='404' element = {<Page404 />}></Route>
     </Routes>
   </BrowserRouter>
 );
