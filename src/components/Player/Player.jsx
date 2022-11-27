a7a680e21456758ccff08542b1d8f826c58689bc
robert <robert.frundin@gmail.com>
2022-11-27 04:29:14 +0300

    added player hash

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -4,22 +4,33 @@ import avatar2 from "../../assets/avatars/2.svg";
 import avatar3 from "../../assets/avatars/3.svg";
 import avatar4 from "../../assets/avatars/4.svg";
 import avatar5 from "../../assets/avatars/5.svg";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import styles from "./player.module.scss";
-// import medal from "../../assets/medal.svg";
-const images = [avatar0, avatar1, avatar2, avatar3, avatar4, avatar5];
-
 export const Player = ({ player }) => {
-  const playerImage = useMemo(() => images[Math.round(Math.random() * 5)]);
-  // const imageLink = useMemo(() => setsrc('../../assets/avatars/'+ Math.floor(Math.random() * 6) + '.svg');
+  const images = [avatar0, avatar1, avatar2, avatar3, avatar4, avatar5];
+  const getHash = function (str) {
+    var hash = 0,
+      i,
+      chr;
+    if (str.length === 0) return hash;
+    for (i = 0; i < str.length; i++) {
+      chr = str.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
+
+  const [src, setSrc] = useState(images[Math.abs(getHash(player.id) % 6)]);
+
   return (
     <li className={styles.wrap}>
-      <img src={playerImage} alt="" className={styles.avatar} />
+      <img src={src} alt="" className={styles.avatar} />
       <div className={styles.playerinfo}>
         <p>{player.name}</p>
         <span>{player.score}</span>
       </div>
-      {/* <img src={medal} alt="" className={styles.placeimg} /> */}
+      {/*<img src={medal} alt="" className={styles.placeimg} />*/}
     </li>
   );
 };
